67c16f25bd9fe98f528c2685ff30d4a0f48a3d07
jirka <jiritresohlavy2@gmail.com>
2026-03-10 00:58:12 +0100

    data_load

--- src/types/dto/load/data_loading_input_dto.py
+++ src/types/dto/load/data_loading_input_dto.py
@@ -6,6 +6,7 @@ from pathlib import Path
 class DataLoadingInputDTO:
     dataset_name: str
     dataset_path: Path
-    # subject_ids: list[int] -- commented out for better understanding (not that important field)
-    # session_ids: list[int] | None -- commented out for better understanding (not that important field)
-    # run_ids: list[int] | None -- commented out for better understanding (not that important field)
+    subject_ids: list[int] # commented out for better understanding (not that important field)
+    session_ids: list[int] | None # commented out for better understanding (not that important field)
+    run_ids: list[int] | None # commented out for better understanding (not that important field)
+    task: str
