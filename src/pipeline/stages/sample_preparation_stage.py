e7580f114b25e0d298615a6ba9c6a2e02a1df40f
jirka <jiritresohlavy2@gmail.com>
2026-03-10 00:56:37 +0100

    sample prep stage

---
+++ src/pipeline/stages/sample_preparation_stage.py
@@ -0,0 +1,11 @@
+from src.types.dto.training.prepared_samples_dto import PreparedSamplesDTO
+from src.types.dto.training.sample_preparation_input_dto import SamplePreparationInputDTO
+from src.types.interfaces.sample_preparer import ISamplePreparer
+
+
+class SamplePreparationStage:
+    def __init__(self, sample_preparer: ISamplePreparer) -> None:
+        self._sample_preparer = sample_preparer
+
+    def run(self, input_dto: SamplePreparationInputDTO) -> PreparedSamplesDTO:
+        return self._sample_preparer.run(input_dto)
