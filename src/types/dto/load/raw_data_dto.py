f7b6709b5074c5a0174a70f70505657d7f27a1ef
jirka <jiritresohlavy2@gmail.com>
2026-03-09 22:28:07 +0100

    finished validation

---
+++ src/types/dto/load/raw_data_dto.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass(frozen=True)
+class RawDataDTO:
+    signal: Any
+    """
+    Loaded EEG signal
+    """
+
+    sampling_freq: float
+    """
+    Sampling frequency of signal. How often the signal was sampled per one second
+    """
+
+    channel_names: list[str]
+    """
+    List of EEG channel names
+    """
+
+    metadata: dict[str, object]
+    """
+    Optional container for information about loaded data (dataset name, run id, etc.)
+    """
