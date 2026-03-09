0feb5df26ea80ed3393680af9089a8633b384e01
jirka <jiritresohlavy2@gmail.com>
2026-03-09 22:55:09 +0100

    raw_data_validation interface

---
+++ src/types/interfaces/raw_data_validator.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+from src.types.dto.load.raw_data_dto import RawDataDTO
+from src.validation.contracts.validation_result import ValidationResult
+
+
+class IRawDataValidator(ABC):
+    @abstractmethod
+    def validate(self, raw_data: RawDataDTO) -> ValidationResult[RawDataDTO]:
+        raise NotImplementedError
