1090aeca2a169ff473b04fe0ac791501c615c3a5
jirka <jiritresohlavy2@gmail.com>
2026-03-09 20:10:08 +0100

    preprocessing data types

---
+++ src/types/dto/preprocessing/preprocessing_input_dto.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from src.types.dto.raw_data_dto import RawDataDTO
+
+
+@dataclass(frozen=True)
+class PreprocessingInputDTO:
+    raw_data: RawDataDTO
+    l_freq: float | None
+    h_freq: float | None
+    notch_freq: float | None
+    target_s_freq: float | None
+    channel_selection: list[str] | None
