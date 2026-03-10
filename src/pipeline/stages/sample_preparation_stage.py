08073eaa634f69132d561a704f7df36796bf010e
jirka <jiritresohlavy2@gmail.com>
2026-03-10 01:21:51 +0100

    run context to interfaces as dep

--- src/pipeline/stages/sample_preparation_stage.py
+++ src/pipeline/stages/sample_preparation_stage.py
@@ -1,3 +1,4 @@
+from src.pipeline.context.run_context import RunContext
 from src.types.dto.training.prepared_samples_dto import PreparedSamplesDTO
 from src.types.dto.training.sample_preparation_input_dto import SamplePreparationInputDTO
 from src.types.interfaces.sample_preparer import ISamplePreparer
@@ -7,5 +8,5 @@ class SamplePreparationStage:
     def __init__(self, sample_preparer: ISamplePreparer) -> None:
         self._sample_preparer = sample_preparer
 
-    def run(self, input_dto: SamplePreparationInputDTO) -> PreparedSamplesDTO:
-        return self._sample_preparer.run(input_dto)
+    def run(self, input_dto: SamplePreparationInputDTO, run_ctx: RunContext) -> PreparedSamplesDTO:
+        return self._sample_preparer.run(input_dto, run_ctx)
