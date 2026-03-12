072461e1a0f87343f486a8fd13b967ad77aadf6e
jirka <jiritresohlavy2@gmail.com>
2026-03-12 17:56:51 +0100

    prpeprocessing dummy

--- src/impl/preprocessing/dummy_preprocessing.py
+++ src/impl/preprocessing/dummy_preprocessing.py
@@ -1,3 +1,5 @@
+import logging
+
 from src.pipeline.context.run_context import RunContext
 from src.types.dto.preprocessing.preprocessed_data_dto import PreprocessedDataDTO
 from src.types.dto.preprocessing.preprocessing_input_dto import PreprocessingInputDTO
@@ -6,6 +8,8 @@ from src.types.interfaces.preprocessing import IPreprocessing
 
 class DummyPreprocessing(IPreprocessing):
     def run(self, input_dto: PreprocessingInputDTO, run_ctx: RunContext) -> PreprocessedDataDTO:
+        log = logging.getLogger(__name__)
+        log.info("Running dummy preprocessing")
         return PreprocessedDataDTO(signal="",
                                    s_freq=128.0,
                                    channel_names=["channel1", "channel2", "channel3"])
