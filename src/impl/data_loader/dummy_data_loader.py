45707bbd625d12bbc6fe76e4bb66236bba0a8efa
jirka <jiritresohlavy2@gmail.com>
2026-03-12 16:14:02 +0100

    dummy loader repaired

--- src/impl/data_loader/dummy_data_loader.py
+++ src/impl/data_loader/dummy_data_loader.py
@@ -1,10 +1,10 @@
-from pipeline.context.run_context import RunContext
-from types.interfaces.data_loader import IDataLoader
-from types.dto.load.data_loading_input_dto import DataLoadingInputDTO
-from types.dto.load.raw_data_dto import RawDataDTO
+from src.pipeline.context.run_context import RunContext
+from src.types.dto.load.data_loading_input_dto import DataLoadingInputDTO
+from src.types.dto.load.raw_data_dto import RawDataDTO
+from src.types.interfaces.data_loader import IDataLoader
+
 
 class DummyLoader(IDataLoader):
     def run(self, input_dto: DataLoadingInputDTO, run_ctx: RunContext) -> RawDataDTO:
-        data = RawDataDTO()
-        data.sampling_freq = 42
+        data = RawDataDTO(sampling_freq=42, channel_names=[], signal="")
         return data
