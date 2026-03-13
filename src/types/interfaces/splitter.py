8cfd9aa59199da42d0570eef469a23497c4ccde0
jirka <jiritresohlavy2@gmail.com>
2026-03-13 15:58:24 +0100

    splitter

--- src/types/interfaces/splitter.py
+++ src/types/interfaces/splitter.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
 
+from src.pipeline.context.run_context import RunContext
+from src.pipeline.contracts.step_result import StepResult
+from src.types.dto.split.dataset_split_dto import DatasetSplitDTO
+from src.types.dto.split.split_input_dto import SplitInputDTO
+
 
 class ISplitter(ABC):
+
     @abstractmethod
-    def run(self, input_dto: PreprocessingInputDTO, run_ctx: RunContext) -> StepResult[PreprocessedDataDTO]:
+    def run(
+            self,
+            input_dto: SplitInputDTO,
+            run_ctx: RunContext,
+    ) -> StepResult[DatasetSplitDTO]:
         raise NotImplementedError
