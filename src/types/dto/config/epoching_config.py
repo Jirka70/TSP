33977229c0d565ca41d43122a9e68ab13deeb9a0
jirka <jiritresohlavy2@gmail.com>
2026-03-13 13:52:52 +0100

    epoching config type

--- src/types/dto/config/epoching_config.py
+++ src/types/dto/config/epoching_config.py
@@ -1,11 +1,50 @@
 from dataclasses import dataclass
+from typing import Literal
 
 
 @dataclass(frozen=True)
-class DatasetConfig:
-    name: str
-    path: Path
-    subject_ids: list[int]
-    session_ids: list[int] | None
-    run_ids: list[int] | None
-    task: str
+class EpochingConfig:
+    """
+    Configuration of epoching step.
+    """
+
+    enabled: bool
+    backend: str
+
+    event_source: Literal["annotations", "stim_channel"]
+    """
+    Where event declarations come from
+    """
+
+    event_mapping: dict[str, int]
+    """
+    Mapping of event names to event ID in data
+    E.g. {"left_hand": 2, "right_hand": 3}
+    """
+
+    event_labels: dict[str, int]
+    """
+    Mapping of event name on classification label (for model-training purposes)
+    E.g. {"left_hand": 0, "right_hand": 1}
+    """
+
+    tmin: float
+    tmax: float
+    """
+    Start and end time of epoch relative to event
+    """
+
+    baseline: tuple[float, float] | None
+    """
+    Např. (-0.2, 0.0) nebo None.
+    """
+
+    preload: bool
+    reject_by_annotation: bool
+    drop_last_incomplete_epoch: bool
+    skip_missing_events: bool
+
+    picks: list[str] | None
+    """
+    Optional channel pick
+    """
--- src/types/dto/config/experiment_config.py
+++ src/types/dto/config/experiment_config.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from src.types.dto.config.augmentation_config import AugmentationConfig
 from src.types.dto.config.dataset_config import DatasetConfig
+from src.types.dto.config.epoching_config import EpochingConfig
 from src.types.dto.config.evaluation_config import EvaluationConfig
 from src.types.dto.config.mode import Mode
 from src.types.dto.config.model_config import ModelConfig
@@ -12,6 +13,7 @@ from src.types.dto.config.preprocessing_config import PreprocessingConfig
 class ExperimentConfig:
     dataset: DatasetConfig
     preprocessing: PreprocessingConfig
+    epoching: EpochingConfig
     augmentation: AugmentationConfig
     model: ModelConfig
     evaluation: EvaluationConfig
