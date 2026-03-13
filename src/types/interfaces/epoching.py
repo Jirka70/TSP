ac61623fae4a13f7af3de7e487ecc1f406670969
jirka <jiritresohlavy2@gmail.com>
2026-03-13 14:24:12 +0100

    delete redundant stagee

--- src/types/interfaces/epoching.py
+++ src/types/interfaces/epoching.py
@@ -5,10 +5,10 @@ from abc import ABC, abstractmethod
 from src.pipeline.context.run_context import RunContext
 from src.pipeline.contracts.step_result import StepResult
 from src.types.dto.epoching.epoching_input_dto import EpochingInputDTO
+from src.types.dto.epoching.epochs_data_dto import EpochsDataDTO
 
 
 class IEpoching(ABC):
-
 
     @abstractmethod
     def name(self) -> str:
@@ -16,8 +16,8 @@ class IEpoching(ABC):
 
     @abstractmethod
     def run(
-        self,
-        input_dto: EpochingInputDTO,
-        run_ctx: RunContext,
+            self,
+            input_dto: EpochingInputDTO,
+            run_ctx: RunContext,
     ) -> StepResult[EpochsDataDTO]:
         raise NotImplementedError
