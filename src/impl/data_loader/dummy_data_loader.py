6c3c884819a0e52e918e8b4967c85577ccf9c048
DavidSt29 <davidstraka29@gmail.com>
2026-04-04 10:49:31 +0200

    Added implementation in dummy loader to test the preprocessing stages.

--- src/impl/data_loader/dummy_data_loader.py
+++ src/impl/data_loader/dummy_data_loader.py
@@ -1,15 +1,31 @@
 import logging
 
+from moabb.datasets import BNCI2014_001
+
 from src.pipeline.context.run_context import RunContext
 from src.pipeline.contracts.step_result import StepResult
 from src.types.dto.config.dataset_config import DatasetConfig
-from src.types.dto.load.raw_data_dto import RawDataDTO
+from src.types.dto.raw_preprocessing.raw_preprocessing_input_dto import RawPreprocessingInputDto
 from src.types.interfaces.data_loader import IDataLoader
 
 
 class DummyLoader(IDataLoader):
-    def run(self, input_dto: DatasetConfig, run_ctx: RunContext) -> StepResult[RawDataDTO]:
+    def run(self, input_dto: DatasetConfig, run_ctx: RunContext) -> StepResult[RawPreprocessingInputDto]:
         log = logging.getLogger(__name__)
-        log.info("Running dummy data loader")
-        data = RawDataDTO(sampling_freq=42, channel_names=[], signal="")
-        return StepResult(data, None, [])
+        log.info("Loading MOABB dataset as mne.Raw")
+
+        dataset = BNCI2014_001()
+        subject_id = 1
+
+        data_dict = dataset.get_data(subjects=[subject_id])
+
+        sessions = data_dict[subject_id]
+        first_session_key = next(iter(sessions))
+        runs = sessions[first_session_key]
+        first_run_key = next(iter(runs))
+
+        mne_raw = runs[first_run_key]
+
+        result_dto = RawPreprocessingInputDto(raw_preprocessing_config=None, signal=mne_raw)
+
+        return StepResult(result_dto, None, [])
