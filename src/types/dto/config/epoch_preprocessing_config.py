06084686bc725c3dd2eb79035c2fb6130687e49d
matyasvelc <matyas.velc@gmail.com>
2026-05-02 13:30:24 +0200

    Commit: Edit for epoch preproccesing - adding funcionalities.
    Task: TSMPKED-386

--- src/types/dto/config/epoch_preprocessing_config.py
+++ src/types/dto/config/epoch_preprocessing_config.py
@@ -5,14 +5,28 @@ from pydantic import BaseModel
 from src.types.dto.config.astageconfig import AStageConfig
 
 
+class AlignmentConfig(BaseModel):
+    enabled: bool
+    tmin_offset: float
+
+
 class ICAConfig(BaseModel):
+    enabled: bool
     n_components: int
     random_state: int
     method: Literal["fastica", "infomax", "picard"]
     eog_threshold: float
 
 
+class AutoRejectConfig(BaseModel):
+    enabled: bool
+    n_interpolate: list[int]
+    consensus: list[float]
+    cv: int
+
+
 class CSPConfig(BaseModel):
+    enabled: bool
     n_components: int
     reg: float | str | None = None
     log: bool
@@ -21,5 +35,7 @@ class CSPConfig(BaseModel):
 
 class EpochPreprocessingConfig(AStageConfig):
     backend: Literal["testing"]
+    alignment: AlignmentConfig
     ica: ICAConfig
+    autoreject: AutoRejectConfig
     csp: CSPConfig
