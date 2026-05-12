aab232f18b2896c0df1364df01c0cdaa55f0d460
jirka <jiritresohlavy2@gmail.com>
2026-05-13 00:54:33 +0200

    Message: code cleanup
    Task: TMSPKED-418

--- src/impl/model/util/serialize/write_json.py
+++ src/impl/model/util/serialize/write_json.py
@@ -2,13 +2,18 @@ import json
 from pathlib import Path
 from typing import Any
 
+from src.impl.model.util.serialize.to_jsonable import to_jsonable
+
+MODE = "w"
+ENDODING = "utf-8"
+
 
 def write_json(file_path: Path, value: Any) -> None:
     file_path.parent.mkdir(parents=True, exist_ok=True)
 
-    with file_path.open("w", encoding="utf-8") as file:
+    with file_path.open(MODE, encoding=ENDODING) as file:
         json.dump(
-            self._to_jsonable(value),
+            to_jsonable(value),
             file,
             indent=2,
             ensure_ascii=False,
