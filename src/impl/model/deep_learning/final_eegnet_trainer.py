b4daf94363f4c6fd2cd04e561e0141defc9a6feb
jirka <jiritresohlavy2@gmail.com>
2026-05-16 18:04:52 +0200

    Message: prepared fold model removed (unnecessary)
    Task: TMSPKED-418

--- src/impl/model/deep_learning/final_eegnet_trainer.py
+++ src/impl/model/deep_learning/final_eegnet_trainer.py
@@ -49,6 +49,47 @@ def extract_final_training_data_from_folds(
     return extract_learning_data(EpochPreprocessedDTO(data=unique_recordings))
 
 
+def _evaluate_model(
+        model: EEGNetModel,
+        x_validation: np.ndarray | None,
+        y_validation: np.ndarray | None,
+) -> float:
+    if x_validation is None or y_validation is None:
+        log.warning("Validation data are not present. Model will not be validated")
+        return NO_ACCURACY
+
+    _, validation_accuracy = model.evaluate(x_validation, y_validation)
+
+    return validation_accuracy
+
+
+def _log_epoch_metrics(
+        model: EEGNetModel,
+        epoch: int,
+) -> None:
+    if model.history is None:
+        log.warning("Epoch %s has no training history to summarize.", epoch + 1)
+        return
+
+    train_accuracy = model.history.train_metrics.get("accuracy", [])
+    if train_accuracy:
+        log.info(
+            "Epoch %s train accuracy=%s",
+            epoch + 1,
+            train_accuracy[-1],
+        )
+
+
+def _train_one_epoch(
+        model: EEGNetModel,
+        x_train: np.ndarray,
+        y_train: np.ndarray,
+        epoch: int,
+) -> None:
+    log.info("Epoch %s train", epoch + 1)
+    model.train_one_epoch(x_train, y_train)
+
+
 class FinalEEGNetTrainer(IFinalTrainer):
     """
     Trains one final EEGNet model.
@@ -65,8 +106,6 @@ class FinalEEGNetTrainer(IFinalTrainer):
     ) -> StepResult[FinalTrainingResultDTO]:
         if not input_dto.folds:
             raise ValueError("Final EEGNet training needs at least one fold.")
-
-
 
         epochs = input_dto.config.training.epochs
         x_train, y_train = extract_final_training_data_from_folds(input_dto.folds)
@@ -89,10 +128,10 @@ class FinalEEGNetTrainer(IFinalTrainer):
 
         for epoch in range(epochs):
             log.info("Epoch %s/%s started", epoch + 1, epochs)
-            self._train_one_epoch(model, x_train, y_train, epoch)
-            self._log_epoch_metrics(model, epoch)
+            _train_one_epoch(model, x_train, y_train, epoch)
+            _log_epoch_metrics(model, epoch)
             log.info("Epoch %s evaluate validation data", epoch + 1)
-            validation_accuracy = self._evaluate_model(model, x_validation, y_validation)
+            validation_accuracy = _evaluate_model(model, x_validation, y_validation)
             log.info(
                 "Epoch %s validation accuracy=%s",
                 epoch + 1,
@@ -120,44 +159,3 @@ class FinalEEGNetTrainer(IFinalTrainer):
                 trained_model=trained_model,
             )
         )
-
-    def _train_one_epoch(
-            self,
-            model: EEGNetModel,
-            x_train: np.ndarray,
-            y_train: np.ndarray,
-            epoch: int,
-    ) -> None:
-        log.info("Epoch %s train", epoch + 1)
-        model.train_one_epoch(x_train, y_train)
-
-    def _log_epoch_metrics(
-            self,
-            model: EEGNetModel,
-            epoch: int,
-    ) -> None:
-        if model.history is None:
-            log.warning("Epoch %s has no training history to summarize.", epoch + 1)
-            return
-
-        train_accuracy = model.history.train_metrics.get("accuracy", [])
-        if train_accuracy:
-            log.info(
-                "Epoch %s train accuracy=%s",
-                epoch + 1,
-                train_accuracy[-1],
-            )
-
-    def _evaluate_model(
-            self,
-            model: EEGNetModel,
-            x_validation: np.ndarray | None,
-            y_validation: np.ndarray | None,
-    ) -> float:
-        if x_validation is None or y_validation is None:
-            log.warning("Validation data are not present. Model will not be validated")
-            return NO_ACCURACY
-
-        _, validation_accuracy = model.evaluate(x_validation, y_validation)
-
-        return validation_accuracy
