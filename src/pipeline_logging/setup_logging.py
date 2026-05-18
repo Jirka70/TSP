1625220dad4ec53ea9d3e7656587df786d299b5c
jirka <jiritresohlavy2@gmail.com>
2026-05-18 18:05:04 +0200

    Message: still setupping...
    Task: TMSPKED-430

--- src/pipeline_logging/setup_logging.py
+++ src/pipeline_logging/setup_logging.py
@@ -9,16 +9,13 @@ from src.types.dto.config.logging.logging_config import LoggingConfig
 from logging import Formatter
 
 
-INFO_LEVEL: int = logging.INFO
 DEBUG_LEVEL = logging.DEBUG
-WARNING_LEVEL = logging.WARNING
 ENCODING = "utf-8"
 
 def setup_pipeline_logging(config: LoggingConfig) -> None:
     root_logger = logging.getLogger()
-    root_logger.setLevel(INFO_LEVEL)
-
     root_logger.handlers.clear()
+    root_logger.setLevel(DEBUG_LEVEL)
 
     formatter: Formatter
 
@@ -33,19 +30,14 @@ def setup_pipeline_logging(config: LoggingConfig) -> None:
     pipeline_logger = logging.getLogger("pipeline")
     pipeline_logger.handlers.clear()
     pipeline_logger.setLevel(DEBUG_LEVEL)
-    pipeline_logger.propagate = False
+    pipeline_logger.propagate = True
 
     if config.console:
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(formatter)
         console_handler.setLevel(DEBUG_LEVEL)
         console_handler.addFilter(PipelineOrWarningFilter())
-        pipeline_logger.addHandler(console_handler)
-
-        root_console_handler = logging.StreamHandler(sys.stdout)
-        root_console_handler.setFormatter(formatter)
-        root_console_handler.setLevel(WARNING_LEVEL)
-        root_logger.addHandler(root_console_handler)
+        root_logger.addHandler(console_handler)
 
     if config.file:
         if config.file_path is None or config.file_path.strip() == "":
@@ -58,9 +50,4 @@ def setup_pipeline_logging(config: LoggingConfig) -> None:
         file_handler.setFormatter(formatter)
         file_handler.addFilter(PipelineOrWarningFilter())
         file_handler.setLevel(DEBUG_LEVEL)
-        pipeline_logger.addHandler(file_handler)
-
-        root_file_handler = logging.FileHandler(log_file_path, encoding=ENCODING)
-        root_file_handler.setFormatter(formatter)
-        root_file_handler.setLevel(WARNING_LEVEL)
-        root_logger.addHandler(root_file_handler)
+        root_logger.addHandler(file_handler)
