ca52ef3622614f87e537a521e6431f3051cc8a7b
jirka <jiritresohlavy2@gmail.com>
2026-05-18 16:28:40 +0200

    Message:verbosity order
    Task: TMSPKED-418

--- src/types/dto/config/logging/verbosity_level.py
+++ src/types/dto/config/logging/verbosity_level.py
@@ -6,3 +6,10 @@ class VerbosityLevel(IntEnum):
     NORMAL = 1
     DETAILED = 2
     TRACE = 3
+
+VERBOSITY_ORDER = {
+    VerbosityLevel.QUIET: 1,
+    VerbosityLevel.NORMAL: 2,
+    VerbosityLevel.DETAILED: 3,
+    VerbosityLevel.TRACE: 4,
+}
