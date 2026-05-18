349d755f8cab517d96655d7bc6f5cd23901f722f
jirka <jiritresohlavy2@gmail.com>
2026-05-18 16:29:17 +0200

    Message:verbosity order
    Task: TMSPKED-418

--- src/types/dto/config/logging/verbosity_level.py
+++ src/types/dto/config/logging/verbosity_level.py
@@ -1,11 +1,12 @@
-from enum import IntEnum
+from enum import StrEnum
 
 
-class VerbosityLevel(IntEnum):
-    QUIET = 0
-    NORMAL = 1
-    DETAILED = 2
-    TRACE = 3
+class VerbosityLevel(StrEnum):
+    QUIET = "quiet"
+    NORMAL = "normal"
+    DETAILED = "detailed"
+    TRACE = "trace"
+
 
 VERBOSITY_ORDER = {
     VerbosityLevel.QUIET: 1,
