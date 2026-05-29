7da84aab86160b3a058cf6edf8d0bca588185a61
jirka <jiritresohlavy2@gmail.com>
2026-05-29 14:53:26 +0200

    Message: synthetic data loader
    Task: TMSPKED-380

--- src/types/dto/config/source/SyntheticDatasetConfig.py
+++ src/types/dto/config/source/SyntheticDatasetConfig.py
@@ -4,7 +4,7 @@ from src.types.dto.config.astageconfig import AStageConfig
 
 class SyntheticDatasetConfig(AStageConfig):
     backend: Literal["synthetic"]
-    s_freq: float = 128.0
+    sfreq: float = 128.0
     n_channels: int = 8
     n_trials: int = 24
     random_seed: int = 42
